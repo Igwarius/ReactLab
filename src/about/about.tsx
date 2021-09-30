b595834769676240188e589b714574b4ab8981be
Igor <ejenzolotnik@mail.ru>
2021-09-30 18:36:53 +0300

    create about page

---
+++ src/about/about.tsx
@@ -0,0 +1,6 @@
+// watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
+
+function About() {
+  return <div>About</div>;
+}
+export default About;
