6d3be2841173d534f9447f26b3b7a5648696dd0e
Igor <ejenzolotnik@mail.ru>
2021-09-30 18:39:41 +0300

    Add Header and footer to main file and config router

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import "./styles/main.scss";
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
 import { Component } from "react";
 import ReactDom from "react-dom";
+import { createBrowserHistory } from "history";
+import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import Products from "./products/products";
+import Main from "./mainPage/mainPage";
+import urls from "./statics/urls";
+import About from "./about/about";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 interface AppProps {
   nothing: boolean;
@@ -14,8 +22,23 @@ interface AppState {
 class AppContainer extends Component<AppProps, AppState> {
   ["constructor"]: typeof AppContainer;
 
+  history = createBrowserHistory();
+
   render() {
-    return <span>Hello world</span>;
+    return (
+      <Router>
+        <Header />
+        <div className="page">
+          <Switch>
+            <Route exact path={urls.MAIN} component={Main} />
+            <Route exact path={urls.PRODUCTS} component={Products} />
+            <Route exact path={urls.ABOUT} component={About} />
+            <Route render={() => <Redirect to={urls.MAIN} />} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    );
   }
 }
 
