ff6ec3571788ee39c9719cc38405e74692313c7d
Igor <ejenzolotnik@mail.ru>
2021-09-30 18:37:20 +0300

    create main page

---
+++ src/mainPage/mainPage.tsx
@@ -0,0 +1,6 @@
+// watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
+
+function Main() {
+  return <div>Main</div>;
+}
+export default Main;
