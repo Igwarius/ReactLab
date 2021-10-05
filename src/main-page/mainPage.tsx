270dbc266645123db590e5df1768f5d69c7cbec1
Igor <ejenzolotnik@mail.ru>
2021-10-05 11:51:33 +0300

    change to AF in mainPage.tsx

--- src/main-page/mainPage.tsx
+++ src/main-page/mainPage.tsx
@@ -1,6 +1,4 @@
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
 
-function Main() {
-  return <div>Main</div>;
-}
+const Main = () => <div>Main</div>;
 export default Main;
