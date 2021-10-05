fcc853f0b2366d7fcad7a6d025dfd16f02e652a9
Igor <ejenzolotnik@mail.ru>
2021-10-06 00:37:21 +0300

    delete comment

--- src/main-page/mainPage.tsx
+++ src/main-page/mainPage.tsx
@@ -1,4 +1,2 @@
-// watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
-
 const Main = () => <div>Main</div>;
 export default Main;
