568cbeebcb5c2c363433d892a959960435331329
Igor <ejenzolotnik@mail.ru>
2021-11-26 12:45:52 +0300

    delete reduent interfaces

--- src/Redux/selectors/authSelectors.ts
+++ src/Redux/selectors/authSelectors.ts
@@ -1,13 +1,4 @@
-interface IAuthIsAutorised {
-  auth: {
-    isAutorised: boolean;
-  };
-}
+import { RootState } from "../reduxStore";
 
-interface IAuthStatus {
-  auth: {
-    status: number;
-  };
-}
-export const isAutorisedSelector = (state: IAuthIsAutorised) => state.auth.isAutorised;
-export const getStatusSelector = (state: IAuthStatus) => state.auth.status;
+export const isAutorisedSelector = (state: RootState) => state.auth.isAutorised;
+export const getStatusSelector = (state: RootState) => state.auth.status;
--- src/Redux/selectors/gameSelectors.ts
+++ src/Redux/selectors/gameSelectors.ts
@@ -1,16 +1,4 @@
-import { IGame } from "@/types";
+import { RootState } from "../reduxStore";
 
-interface IGamesThree {
-  game: {
-    items: IGame[];
-  };
-}
-
-interface IGamesSearch {
-  game: {
-    searchGames: IGame[];
-  };
-}
-
-export const getThreeGamesSelector = (state: IGamesThree) => state.game.items;
-export const getSearchGamesSelector = (state: IGamesSearch) => state.game.searchGames;
+export const getThreeGamesSelector = (state: RootState) => state.game.items;
+export const getSearchGamesSelector = (state: RootState) => state.game.searchGames;
