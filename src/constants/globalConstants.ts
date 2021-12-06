58217c3a9ed36b7e146b1dc86272ddcf6f599606
Igor <ejenzolotnik@mail.ru>
2021-12-06 16:23:36 +0300

    change enums

--- src/constants/globalConstants.ts
+++ src/constants/globalConstants.ts
@@ -1,26 +1,26 @@
 export const IS_AUTHORIZED_KEY = "isAutorised";
 export const REQUIRED_ERROR_MESSAGE = "Required";
-export const enum ModalType {
+export enum ModalType {
   Registration = "Registration",
   LogIn = "Log in",
   PasswordChange = "Password Change",
 }
 
-export const enum InputName {
+export enum InputName {
   LogIn = "login",
   Password = "password",
   PasswordCheck = "passwordCheck",
   description = "Description",
 }
 
-export const enum InputPlaceholder {
+export enum InputPlaceholder {
   LogIn = "Login",
   Password = "Password",
   PasswordCheck = "Password Check",
   Description = "Description",
 }
 
-export const enum InputType {
+export enum InputType {
   Text = "text",
   Password = "password",
 }
