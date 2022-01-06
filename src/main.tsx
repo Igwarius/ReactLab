f2fd60b64e58e4e938b87cfd99685bf856e74081
Igor <ejenzolotnik@mail.ru>
2022-01-06 13:50:29 +0300

    add lazy loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,42 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./styles/index.css";
 import ReactDom from "react-dom";
 import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
-import Products from "./components/products/Products";
-import Main from "./components/main-page/MainPage";
+
 import urls from "./constants/urls";
-import About from "./components/about/About";
+
 import HeaderContainer from "./components/header/HeaderContainer";
 import Footer from "./components/footer/Footer";
 import ErrorBoundary from "./components/error-boundary/ErrorrBoundary";
 import { store } from "./redux/reduxStore";
-import ProfileContainer from "./components/profile/ProfileContainer";
-import CartContainer from "./components/cart/CartContainer";
+
+const Main = React.lazy(() => import("./components/main-page/MainPage"));
+const Products = React.lazy(() => import("./components/products/Products"));
+const About = React.lazy(() => import("./components/about/About"));
+const ProfileContainer = React.lazy(() => import("./components/profile/ProfileContainer"));
+const CartContainer = React.lazy(() => import("./components/cart/CartContainer"));
 
 const AppContainer = (): JSX.Element => (
   <ErrorBoundary>
-    <Provider store={store}>
-      <Router>
-        <HeaderContainer />
+    <Suspense fallback={<div>Loading... </div>}>
+      <Provider store={store}>
+        <Router>
+          <HeaderContainer />
 
-        <Switch>
-          <Route exact path={urls.MAIN} component={Main} />
-          <Route path={urls.PRODUCTS} component={Products} />
-          <Route exact path={urls.ABOUT} component={About} />
-          <Route exact path={urls.PROFILE} component={ProfileContainer} />
-          <Route exact path={urls.CART} component={CartContainer} />
+          <Switch>
+            <Route exact path={urls.MAIN} component={Main} />
+            <Route path={urls.PRODUCTS} component={Products} />
+            <Route exact path={urls.ABOUT} component={About} />
+            <Route exact path={urls.PROFILE} component={ProfileContainer} />
+            <Route exact path={urls.CART} component={CartContainer} />
 
-          <Route render={() => <Redirect to={urls.MAIN} />} />
-        </Switch>
-        <Footer />
-      </Router>
-    </Provider>
+            <Route render={() => <Redirect to={urls.MAIN} />} />
+          </Switch>
+          <Footer />
+        </Router>
+      </Provider>
+    </Suspense>
   </ErrorBoundary>
 );
 
